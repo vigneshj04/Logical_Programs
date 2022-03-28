69a4ce0d3ff3949c2462c0e79ce41360995d6be7
vigneshj04 <vigneshjmax@gmail.com>
2022-03-28 11:58:57 +0530

    Added: Fibonacci Series Of N Element

--- LogicalPrograms.java
+++ LogicalPrograms.java
@@ -3,4 +3,22 @@ package com.logical.programs;
 import java.util.Scanner;
 
 public class LogicalPrograms {
+    static void fibonacciSeries() {
+        int termOne = 0;
+        int termTwo = 1;
+        int term_N = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        term_N = input.nextInt();
+        System.out.println("Fibonacci Series till " + term_N + " terms:");
+        int i = 1;
+        while (i <= term_N) {
+            System.out.printf(termOne + ", ");
+            int termThree = termOne + termTwo;
+            termOne = termTwo;
+            termTwo = termThree;
+            i++;
+        }
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 package com.logical.programs;
 
+import static com.logical.programs.LogicalPrograms.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        fibonacciSeries();
     }
 }
 
