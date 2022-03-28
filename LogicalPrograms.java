bd202a001bc944d39d92e7b6bcfab400f3048f18
vigneshj04 <vigneshjmax@gmail.com>
2022-03-28 12:04:14 +0530

    Added: To Find Number Is Prime Or Not

--- LogicalPrograms.java
+++ LogicalPrograms.java
@@ -21,4 +21,24 @@ public class LogicalPrograms {
         }
     }
 
+
+    static void primeNumber() {
+        int i = 2;
+        int n;
+        boolean prime = true;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number to check :");
+        n = input.nextInt();
+        while (i < n) {
+            if (n % i == 0) {
+                System.out.println(n + " the number is not prime ");
+                prime = false;
+                i++;
+            }
+        }
+        if (prime = true) {
+            System.out.println(n + " the number is prime");
+        }
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,10 +1,21 @@
 package com.logical.programs;
 
 import static com.logical.programs.LogicalPrograms.*;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        fibonacciSeries();
+        Scanner input = new Scanner(System.in);
+        System.out.println("1.Fibonacci Series");
+        System.out.println("2.Prime Number");
+        System.out.println("Enter the choice: ");
+        int choice = input.nextInt();
+        switch (choice) {
+            case 1:
+                fibonacciSeries();
+            case 2:
+                primeNumber();
+        }
+
     }
 }
 
